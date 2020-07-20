f994a25251143c7ecbc322a1a2e6b8553304e1b3
Andreas Isnes Nilsen <andreas.nilsen@sparebank1.no>
2020-07-20 15:42:03 +0200

    added necessary publishing tools

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
 ENV=pipenv
 
+.PHONY: init test egg publish clean
+
+all: init test egg publish
+
+init:
+	@$(ENV) sync
 
 test:
 	@$(ENV) run pytest --cov=minesweeper tests
+
+egg:
+	@$(ENV) run python setup.py sdist bdist_wheel
+
+publish:
+	@$(ENV) run twine check dist/*
+	@$(ENV) run twine upload dist/*
+
+clean:
+	@find . -type f -name ".mypy_cache" -print0 | xargs -r0 -- rm -r
+	@find . -type d -name ".pytest_cache" -print0 | xargs -r0 -- rm -r
+	@find . -type d -name "__pycache__" -print0 | xargs -r0 -- rm -r
+	@find . -type d -name "*.pyc" -print0 | xargs -r0 -- rm -r
+	@rm -rf *.egg-info build dist .coverage
---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from os import environ
+from os.path import join, dirname
+from setuptools import setup, find_packages
+from json import load
+
+def read(fname):
+    with open(join(dirname(__file__), fname)) as f:
+        return f.read()
+
+def metadata(value):
+    with open(join(dirname(__file__), "metadata.json")) as f:
+        return load(f)[value]
+
+setup(
+    # Package
+    name="pygame-minesweeper-sprites",
+    version=metadata("version"),
+    packages=find_packages(exclude=("tests", "screenshots")),
+    url="https://github.com/andreasisnes/Elitekollektivet.Minesweeper.Sprites",
+    install_requires=["pygame"],
+
+    zip_safe=False,
+    include_package_data=True,
+
+    # Contact
+    author="Andreas Isnes Nilsen",
+    author_email="andreas.isnes@gmail.com",
+
+    # Description
+    description="Minesweeper sprites for pygame applications",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
